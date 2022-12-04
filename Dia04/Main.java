8584ae91f3cbf2ee5e6703f9c0c26cca2c0b0af7
Miguel Sabater López (S) <migsabaterlop@gmail.com>
2022-12-04 12:54:44 +0100

    Día cuatro.

---
+++ Dia04/Main.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import static java.lang.Integer.parseInt;
+
+public class Main {
+    private static Pattern P_PAIR = Pattern.compile("(\\d+)-(\\d+),(\\d+)-(\\d+)");
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int cuenta = 0;
+        while (sc.hasNextLine()) {
+            String line = sc.nextLine();
+            Matcher m = P_PAIR.matcher(line);
+            if (m.matches()) {
+                int minUno = parseInt(m.group(1));
+                int maxUno = parseInt(m.group(2));
+                int minDos = parseInt(m.group(3));
+                int maxDos = parseInt(m.group(4));
+                if ((minUno <= minDos && maxUno >= maxDos) || (minUno >= minDos && maxUno <= maxDos)) {
+                    cuenta++;
+                }
+            }
+        }
+        System.out.println(cuenta);
+        sc.close();
+    }
+}
